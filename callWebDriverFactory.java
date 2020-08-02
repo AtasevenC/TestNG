e5cd62f3e761150ec38e1cd52d33b79842f4b06f
Çağrı ATASEVEN <cataseven3443@gmail.com>
2020-08-02 14:26:41 +0200

    BasicWebDriverFactory

---
+++ callWebDriverFactory.java
@@ -0,0 +1,10 @@
+package com.cybertek.tests.day1_basic_navigation;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+
+public class callWebDriverFactory {
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+    }
+}
